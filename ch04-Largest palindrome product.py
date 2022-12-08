2ae3483793d2d0f6ad48b0fcb4960dbc193af079
Mert <77935876+splashoui@users.noreply.github.com>
2022-12-08 19:49:51 +0100

    Update ch04-Largest palindrome product.py

--- ch04-Largest palindrome product.py
+++ ch04-Largest palindrome product.py
@@ -12,22 +12,26 @@ start = time.time()
 
 # Since we are looking for the largest number, we can earn time by looking from highest to lowest.
 
-# Firstly, storing two 3-digit numbers and storing it in list "num".
+# Firstly, storing two 3-digit numbers and storing it in list "num_list".
 
-# Secondly, sorting the results of multipicilation from highest to lowest.
+# Secondly, storing the multiplications of all 3 digit numbers in "multiplications"
+
+# Thirdly, sorting the results of multipicilation from highest to lowest.
 
 # With for loop, taking each number and with if condition we are checking
 
-# If first 3 strings are equal to reversed version of last 3 strings.
+# If first 3 strings are equal to reversed version of last 3 strings printing the output and breaking the loop.
 
-num=[]
-for i in range(999,99,-1):
-    for j in range(999,99,-1):
-        num.append(i*j) 
+num_list = list(range(999,99,-1))
 
-for x in sorted(num,reverse=True):
-    if (str(x)[0:int(len(str(x))/2)]) == (str(x)[-int(len(str(x))/2):][::-1]):
-        print(x)
+multiplications = []
+for n1 in num_list:
+    for n2 in num_list:
+        multiplications.append(n1 * n2)
+        
+for mult in sorted(multiplications,reverse=True):
+    if str(mult)[0:3] == str(mult)[-1:2:-1]:
+        print(mult)
         break
     
 # time at the end of program execution
@@ -35,3 +39,5 @@ end = time.time()
 
 # total time for program execution
 print(f"Execution time: {end - start} ")   
+
+# Execution time: 0.14335203170776367 
