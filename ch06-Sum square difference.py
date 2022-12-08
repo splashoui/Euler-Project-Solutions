4715daf868953853ceb24a6045832ef0e7b2d372
Mert <77935876+splashoui@users.noreply.github.com>
2022-12-08 20:16:59 +0100

    Update ch06-Sum square difference.py

--- ch06-Sum square difference.py
+++ ch06-Sum square difference.py
@@ -8,21 +8,22 @@ import time
 # time at the start of program execution
 start = time.time()
 
-# This is a simple problem, so we can solve this straightforward with 2 for loop operations.
+# This is a simple problem, so we can solve this straightforward with a for loop operation.
 
-# The sum of the squares of the first hundred numbers
+# define the starting point of the numbers.
+num_total, sum_sq = 0 , 0 
 
-a = sum([i**2 for i in range(0,101)])
-
-# The square of the sum of the first hundred numbers
-
-b = sum([i for i in range(0,101)])**2
-
-print(f"The difference between the sum of the squares of the first hundred numbers ")
-print(f"and the square of the sum is {b-a}")
+for num in list(range(1,101)):
+    # The sum of the squares of the first hundred numbers
+    sum_sq = sum_sq + num**2
+    # The sum of the first hundred numbers
+    num_total = num_total  + num
+    
+print(abs(sum_sq - num_total**2))
 
 # time at the end of program execution
 end = time.time()
 
 # total time for program execution
 print(f"Execution time: {end - start} ")   
+# Execution time: 1.5020370483398438e-05
